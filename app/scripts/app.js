3ad8263c3d71c6dc9e7bc8d676af730fd1298923
anurag6792 <anuragv@dynaptsolutions.com>
2017-04-04 12:23:58 +0530

    Initial git commit message

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cubicalLabsApp
+ * @description
+ * # cubicalLabsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cubicalLabsApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'ui.bootstrap'
+  ])
+  .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+  $stateProvider.state({
+    name: 'home',
+    url: '/home',
+    templateUrl: './views/main.html',
+    controller: 'MainCtrl',
+    controllerAs: 'vm'
+  });
+  $urlRouterProvider.otherwise('/home');
+}])
+.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.withCredentials = true;
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+    }
+]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cubicalLabsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cubicalLabsApp
+ */
+angular.module('cubicalLabsApp')
+  .controller('MainCtrl',['$http','$sce', function ($http,$sce) {
+    var vm = this;
+    vm.showoverlay = false;
+    vm.showartistlink = true;
+    vm.showtabs = false;
+    vm.artist={};
+    vm.name = /^jack$/i ;
+    vm.track = /^4$/;
+    vm.getTracks = function(artistForm){
+      console.log(vm.artist);
+      $http({
+        method: 'jsonp',
+        url:$sce.trustAsResourceUrl('http://itunes.apple.com/search?term='+vm.artist.name.toLowerCase()+'&limit='+vm.artist.track)
+        })
+      .then(function(response){
+        console.log(response);
+        vm.artists = response.data.results;
+        vm.showoverlay = false;
+        vm.showartistlink = false;
+        vm.showtabs = true;
+      })
+      .catch(function(error){
+        vm.showoverlay = false;
+        vm.showartistlink = true;
+        vm.showtabs = false;
+        vm.artist={};
+      });
+      artistForm.$setPristine();    
+      artistForm.$setUntouched();
+    }
+  }]);
